981bf851828aff99a2a31b630a2596d8ca8895ed
LeoG123321 <3545gonzalez@gmail.com>
2024-01-29 13:27:43 -0600

    Added more tests

--- Hi == hi.cpp
+++ Hi == hi.cpp
@@ -4,6 +4,8 @@
     "Hi" == "hi"?
 
     Write a function that allows case-insensitive comparison (a.k.a. ordering) of two strings.
+
+    Notes: Differnce in ASCII: 32
 */
 
 #include <iostream>
@@ -12,10 +14,18 @@ using namespace std;
 
 int main()
 {
-    //Differnce in ASCII: 32
+    cout << "Lower Case 'a': " << lowerCase('a') << endl;
+    cout << "Lower Case 'A': " << lowerCase('A') << endl;
+    cout << "Lower Case 'm': " << lowerCase('m') << endl;
+    cout << "Lower Case 'M': " << lowerCase('M') << endl;
+    cout << "Lower Case 'z': " << lowerCase('z') << endl;
+    cout << "Lower Case 'Z': " << lowerCase('Z') << endl;
 
-	cout << strcmp_case_insensitive("Hello", "heLLO");
-	return 0;
-
+	cout << strcmp_case_insensitive("Hello", "heLLO") << endl;
+    cout << strcmp_case_insensitive("aHello", "heLLO") << endl;
+    cout << strcmp_case_insensitive("Hello", "AheLLO") << endl;
+    cout << strcmp_case_insensitive("ZHello", "heLLO") << endl;
+	
+    return 0;
 }
 
